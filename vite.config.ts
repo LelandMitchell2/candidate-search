b826797e1fd86cc632ae202cbc8123fd0a3afb13
Leland Mitchell <leland.a.mitchell2@gmail.com>
2025-03-21 21:03:21 -0400

    added server allow list vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
-});
+
+server: {
+  allowedHosts: ['candidate-search-5kq5.onrender.com'],
+},});
